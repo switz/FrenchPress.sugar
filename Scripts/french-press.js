49fc7fd50d61aff3f881abf40f1f730bd134175e
Daniel Saewitz <switz213@gmail.com>
2012-03-02 17:51:46 -0500

    Return selection to top of document instead of bottom

--- Scripts/french-press.js
+++ Scripts/french-press.js
@@ -8,11 +8,13 @@ action.canPerformWithContext = function(context, outError) {
 action.performWithContext = function(context, outError) {
 	// Set the range to the whole text
 	var range = new Range(0, context.string.length)
-	//set the text selection to the whole text
+	// set the text selection to the whole text
 	var text = context.string.substringWithRange(range);
 	//beautify the text
 	text = beautify(text);
 	//insert!
 	context.selectedRanges = [range];
-	return context.insertTextSnippet(new CETextSnippet(text));
+	context.insertTextSnippet(new CETextSnippet(text));
+	context.selectedRanges = [new Range(0, 0)];
+	return true;
 };
